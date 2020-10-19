e4977607e90e355090a160d17914047cab286d62
Emily Tompsett <etompsett@mka.org>
2020-10-19 14:14:11 -0400

    Add files via upload

--- LinkedList.java
+++ LinkedList.java
@@ -32,9 +32,38 @@ public class LinkedList<T> {
 	return find(element) >= 0;
     }
     //FOR HOMEWORK - add (append) - cases: size = 0, size != 03
-    public void add(T element) { } //append
-    public void add(int index, T element) {}
-    public boolean remove(T element) { return false; }
+    public void add(T element) {
+	Node<T> curr = head;
+	Node<T> newElt = new Node<T>(element);
+	if (size == 0) { head = newElt; size++; return; }
+	while (curr.getNext() != null){ curr = curr.getNext(); }
+	curr.setNext(newElt);
+	size++;
+	return;
+    } 
+    public void insert(int index, T element) {
+	assert(index <= size);
+	Node<T> curr = head;
+	Node<T> newElt = new Node<T>(element);
+	if (index == 0){
+	    newElt.setNext(head);
+	    head = newElt;
+	    size++;
+	    return;
+	}
+	for (int i = 0; i < index - 1; i++){ curr = curr.getNext(); }
+	newElt.setNext(curr.getNext());
+	curr.setNext(newElt);
+	size++;
+	return;
+    }
+    public boolean remove(T element) {
+	int pos = find(element);
+	if (pos == -1) return false;
+	remove(pos);
+	return true;
+    }
+    
     public int find(T element) {
 	Node<T> curr = head;
 	int pos = 0;
@@ -52,8 +81,34 @@ public class LinkedList<T> {
 	}
 	return curr.getElement();
     }
-    public T set(int index, T newVal) { return null; }
-    public void remove(int index) {};
+    public T set(int index, T newVal) {
+	assert(index < size); //affirm that the index exists
+	Node<T> curr = head;
+	for (int i = 0;i < index; i++){
+	    curr = curr.getNext();
+	}
+	//at correct position
+	T temp = curr.getElement();
+	curr.setElement(newVal);
+	return temp;
+    }
+    
+    public T remove(int index) {
+	assert(index < size);
+	Node<T> curr = head;
+	if (index == 0){
+	    T toReturn = head.getElement();
+	    head = head.getNext();
+	    size --;
+	    return toReturn;
+	}
+	for (int i = 0; i < index - 1; i++){ curr = curr.getNext(); }
+	Node<T> toRemove = curr.getNext();
+	curr.setNext(toRemove.getNext());
+	size--;
+	return toRemove.getElement();
+    }
+    
     private void print() {
 	Node<T> curr = head;
 	while (curr.getNext() != null){ 
@@ -63,6 +118,21 @@ public class LinkedList<T> {
 	System.out.println(curr.getElement());
     }
 
+
+
+    public static void main(String[] args){
+	LinkedList<String> lst = new LinkedList<String>();
+	lst.add("A");
+	lst.add("B");
+	lst.add("C");
+	lst.add("D");
+	lst.print();
+	lst.remove("D");
+	lst.print();
+
+
+    }
+    
 }
 
 
